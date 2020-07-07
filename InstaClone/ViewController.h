912afe1430cd4140ccf55b19f0fe01ea4510071e
Frank E. Dadzie <fdadzie20@fb.com>
2020-07-07 00:35:26 -0400

    Initial commit

---
+++ InstaClone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaClone
+//
+//  Created by fdadzie20 on 7/6/20.
+//  Copyright © 2020 fdadzie20. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
